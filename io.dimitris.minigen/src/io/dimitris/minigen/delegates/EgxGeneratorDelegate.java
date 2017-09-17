3bcef09ee6a71fb9f76e93973acc7ff5b4d1b5a3
dkolovos <dkolovos@cs.york.ac.uk>
2017-09-17 21:23:17 +0300

    Added presentation template and support for @verbatim rules

--- io.dimitris.minigen/src/io/dimitris/minigen/delegates/EgxGeneratorDelegate.java
+++ io.dimitris.minigen/src/io/dimitris/minigen/delegates/EgxGeneratorDelegate.java
@@ -1,9 +1,18 @@
 package io.dimitris.minigen.delegates;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URI;
 
+import org.apache.commons.io.FileUtils;
+import org.eclipse.epsilon.common.parse.AST;
+import org.eclipse.epsilon.common.util.AstUtil;
 import org.eclipse.epsilon.egl.EglFileGeneratingTemplateFactory;
+import org.eclipse.epsilon.egl.EglTemplateFactory;
 import org.eclipse.epsilon.egl.EgxModule;
+import org.eclipse.epsilon.egl.dom.GenerationRule;
+import org.eclipse.epsilon.egl.execute.context.IEglContext;
+import org.eclipse.epsilon.eol.exceptions.EolRuntimeException;
 import org.eclipse.epsilon.eol.execute.context.Variable;
 
 import apple.applescript.AppleScriptEngine;
@@ -28,15 +37,43 @@ public class EgxGeneratorDelegate implements IFileGeneratorDelegate {
 		
 		EglFileGeneratingTemplateFactory templateFactory = new EglFileGeneratingTemplateFactory();
 		templateFactory.setOutputRoot(rootDirectoryPath);
-		EgxModule module = new EgxModule(templateFactory);
+		EgxModule module = new EgxModule(templateFactory) {
+			@Override
+			protected GenerationRule createGenerationRule(AST generationRuleAst) {
+				GenerationRule superGenerationRule = super.createGenerationRule(generationRuleAst);
+				
+				if (generationRuleAst.getAnnotationsAst() != null) {
+					return new GenerationRule() {
+						public void generateAll(IEglContext context, EglTemplateFactory templateFactory, EgxModule module) throws EolRuntimeException {
+							String template = (templateBlock == null) ? "" : templateBlock.execute(context, false);
+							String target = targetBlock.execute(context, false);
+							
+							File templateFile = new File(((EglFileGeneratingTemplateFactory) templateFactory).resolveTemplate(template));
+							File targetFile = new File(((EglFileGeneratingTemplateFactory) templateFactory).getOutputRoot(), target);
+						
+							
+							System.out.println(templateFile.getAbsolutePath());
+							System.out.println(targetFile.getAbsolutePath());
+							
+							try {
+								FileUtils.copyFile(templateFile, targetFile);
+							} catch (IOException e) {
+								throw new RuntimeException(e);
+							}
+						};
+					};
+				}
+				else return superGenerationRule;
+			}
+		};
 		module.parse(egx);
-		String selectedFile = null;
-		try {
-			selectedFile = (String) appleScriptEngine.eval("tell application \"Finder\" \n set currentFile to (item 1 of (get selection)) \n return name of currentFile \n end tell");
-		}
-		catch (Exception ex) {}
+		//String selectedFile = null;
+		//try {
+		//	selectedFile = (String) appleScriptEngine.eval("tell application \"Finder\" \n set currentFile to (item 1 of (get selection)) \n return name of currentFile \n end tell");
+		//}
+		//catch (Exception ex) {}
 		module.getContext().getFrameStack().put(Variable.createReadOnlyVariable("dir", new File(rootDirectoryPath)));
-		module.getContext().getFrameStack().put(Variable.createReadOnlyVariable("file", new File(selectedFile)));
+		//module.getContext().getFrameStack().put(Variable.createReadOnlyVariable("file", new File(selectedFile)));
 		module.execute();
 		
 	}
